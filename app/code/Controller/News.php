885ef1b3e2b5278b5614e115f64b28ab65ab1d88
Algirdas <alzarkaitis22@gmail.com>
2022-04-04 18:00:29 +0300

    small news improvements

--- app/code/Controller/News.php
+++ app/code/Controller/News.php
@@ -6,15 +6,23 @@ use Core\ControllerAbstract;
 
 class News extends ControllerAbstract
 {
-    public function show($slug){
-        $new = new \Model\News();
-        $new->loadBySlug($slug);
-        $this->twig->display('news/single.html.twig',['new' => $new]);
+
+    public function index()
+    {
+        self::all();
     }
 
-    public function all(){
+    public function show($slug)
+    {
+        $new = new \Model\News();
+        $new->loadBySlug($slug);
+        $this->twig->display('news/single.html.twig', ['new' => $new]);
+    }
+
+    public function all()
+    {
         $news = new \Model\News();
         $articles = $news->getAllNews();
-        $this->twig->display('news/all.html.twig',['news' => $articles]);
+        $this->twig->display('news/all.html.twig', ['news' => $articles]);
     }
 }
